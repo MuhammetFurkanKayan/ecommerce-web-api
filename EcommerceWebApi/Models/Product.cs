9a531aa5bfa8f28d235b9674037d3de054cac4f6
MuhammetFurkanKayan <kayanfurkan0@gmail.com>
2026-08-15 22:30:06 +0300

    feat: add entity models

---
+++ EcommerceWebApi/Models/Product.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace EcommerceWebApi.Models
+{
+    public class Product
+    {
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public float Price { get; set; }
+        public string Description { get; set; }
+        public int Stock { get; set; }
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+        public required int CategoryId { get; set; }
+        public required Category Category { get; set; }
+    }
+}
---
+++ EcommerceWebApi/Models/User.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Components.Forms;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace EcommerceWebApi.Models
+{
+    public class User
+    {
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+        public string UserName { get; set; }
+        public int Age { get; set; }
+        public string Email { get; set; }
+        public string Address { get; set; } = string.Empty;
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+    }
+}
